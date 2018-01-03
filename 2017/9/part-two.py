0f7ad24be4f83c47c1e43f4a79736f01db250ba9
Tiffany Qi <tiffany19qi@gmail.com>
2018-01-02 16:07:34 -0800

    finished day 9

--- 2017/9/part-two.py
+++ 2017/9/part-two.py
@@ -5,6 +5,7 @@ with open('input.txt') as inputfile:
 
 total_score = 0
 curr_brace = 0
+total_garbage = 0
 garbage = False
 skip = False
 
@@ -12,16 +13,23 @@ for char in groups:
     if skip:
         skip = False
     elif not skip:
+        if garbage:
+            total_garbage += 1
+
         if not garbage and char == '{':
             curr_brace += 1
             total_score += curr_brace
         elif not garbage and char == '}' and curr_brace > 0:
             curr_brace -= 1
+
         elif char == '!':
             skip = True
+            total_garbage -= 1
         elif char == '<':
             garbage = True
         elif char == '>':
             garbage = False
+            total_garbage -= 1
 
-print total_score
+
+print total_garbage
